e7d11addc59565ec1ace8d24a4d01cfa7ab6bebe
Luis Amaro <jrferamaro@gmail.com>
2024-06-19 12:18:12 -0300

    fix: error on docker

--- src/main/java/dev/feramaro/mysiteapi/config/cors/CorsConfig.java
+++ src/main/java/dev/feramaro/mysiteapi/config/cors/CorsConfig.java
@@ -3,7 +3,6 @@ package dev.feramaro.mysiteapi.config.cors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
