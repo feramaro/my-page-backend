5d5ebdcba44ba560750d761292bd6ab1fcf975fd
Luis Amaro <jrferamaro@gmail.com>
2024-06-19 12:04:50 -0300

    fix: cors urls

--- src/main/java/dev/feramaro/mysiteapi/config/cors/CorsConfig.java
+++ src/main/java/dev/feramaro/mysiteapi/config/cors/CorsConfig.java
@@ -1,20 +1,24 @@
 package dev.feramaro.mysiteapi.config.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
-@Profile("dev")
-public class DevCorsConfig {
+public class CorsConfig {
+
+    @Value("#{'${cors.origin-urls}'.split(',')}")
+    private List<String> originUrls;
 
     @Bean
     public CorsConfiguration corsConfiguration() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedOrigins(originUrls);
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         config.setAllowCredentials(true);
