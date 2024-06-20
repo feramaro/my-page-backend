7bd7d4c94f1149d74b213bbc772e2a25fb920f16
Luis Amaro <jrferamaro@gmail.com>
2024-06-20 12:24:50 -0300

    test: cors

--- src/main/java/dev/feramaro/mysiteapi/config/SecurityConfiguration.java
+++ src/main/java/dev/feramaro/mysiteapi/config/SecurityConfiguration.java
@@ -1,13 +1,9 @@
 package dev.feramaro.mysiteapi.config;
 
 import dev.feramaro.mysiteapi.config.middlewares.JwtAuthFilter;
-import dev.feramaro.mysiteapi.config.middlewares.OriginFilter;
 import lombok.AllArgsConstructor;
-import org.apache.catalina.filters.CorsFilter;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,11 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +21,6 @@ public class SecurityConfiguration {
 
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthFilter jwtAuthFilter;
-    private final OriginFilter originFilter;
     private final CorsConfigurationSource corsConfigurationSource;
 
 
@@ -44,7 +35,6 @@ public class SecurityConfiguration {
                             .anyRequest().authenticated();
                 })
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(originFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(
